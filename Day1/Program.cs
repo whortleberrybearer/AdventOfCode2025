5bf075b2781269e2a4406cf143359692ae4b05c5
whortleberrybearer <30476438+whortleberrybearer@users.noreply.github.com>
2025-12-01 21:07:55 +0000

    Part 2

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -8,6 +8,9 @@ foreach (var turn in input)
     var direction = turn[0].ToString();
     var amount = int.Parse(turn.Substring(1));
 
+    result += amount / 100;
+    amount %= 100;
+
     if (direction == "R")
     {
         position += amount;
@@ -15,21 +18,29 @@ foreach (var turn in input)
         while (position > 99)
         {
             position -= 100;
+            result++;
         }
     }
     else if (direction == "L")
     {
+        var isZero = position == 0;
         position -= amount;
 
-        while (position < 0)
+        if (position == 0)
+        {             
+            result++;
+            continue;
+        }
+
+        while (position <= 0)
         {
             position += 100;
-        }
-    }
 
-    if (position == 0)
-    {
-        result++;
+            if (!isZero)
+            {
+                result++;
+            }
+        }
     }
 }
 
