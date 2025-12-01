06569bf99c9c039c7e772482c8ae2de550dece1a
whortleberrybearer <30476438+whortleberrybearer@users.noreply.github.com>
2025-12-01 19:27:43 +0000

    Part1

---
+++ Day1/Program.cs
@@ -0,0 +1,36 @@
+﻿var input = File.ReadAllLines("Input.txt");
+
+var position = 50;
+var result = 0;
+
+foreach (var turn in input)
+{
+    var direction = turn[0].ToString();
+    var amount = int.Parse(turn.Substring(1));
+
+    if (direction == "R")
+    {
+        position += amount;
+
+        while (position > 99)
+        {
+            position -= 100;
+        }
+    }
+    else if (direction == "L")
+    {
+        position -= amount;
+
+        while (position < 0)
+        {
+            position += 100;
+        }
+    }
+
+    if (position == 0)
+    {
+        result++;
+    }
+}
+
+Console.WriteLine(result);
