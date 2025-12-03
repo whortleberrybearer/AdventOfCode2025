2603af2d4a12ef74e2c5848167ef1c8cd265bda9
whortleberrybearer <30476438+whortleberrybearer@users.noreply.github.com>
2025-12-03 06:36:01 +0000

    Day 3 (#3)

---
+++ Day3/Program.cs
@@ -0,0 +1,41 @@
+﻿var input = File.ReadAllLines("Input.txt");
+var result = 0L;
+
+foreach (var bank in input)
+{
+    Console.WriteLine(bank);
+
+    var barreries = bank.Select(b => int.Parse(b.ToString())).Reverse().ToArray();
+    var endIndex = barreries.Length;
+    var batteriesString = string.Empty;
+
+    for (var i = 11; i >= 0; i--)
+    {
+        var (largestValue, largestIndex) = FindLargestValue(barreries, i, endIndex);
+        endIndex = largestIndex;
+
+        batteriesString += largestValue;
+    }
+
+    Console.WriteLine($"Batteries: {batteriesString}");
+    result += long.Parse(batteriesString);
+}
+
+Console.WriteLine(result);
+
+static (int value, int index) FindLargestValue(int[] barreries, int startIndex, int endIndex)
+{
+    var index = 0;
+    var value = 0;
+
+    for (var i = startIndex; i < endIndex; i++)
+    {
+        if (barreries[i] >= value)
+        {
+            index = i;
+            value = barreries[i];
+        }
+    }
+
+    return (value, index);
+}
