4604ba0a3782fc28e0a7286809d168b1e32c4fdd
whortleberrybearer <30476438+whortleberrybearer@users.noreply.github.com>
2025-12-05 16:25:49 +0000

    Part 2

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -1,11 +1,9 @@
-﻿using System.Linq;
-
-var input = File.ReadAllLines("Input.txt");
+﻿var input = File.ReadAllLines("Input.txt");
 
 var freshIngredientIds = input.TakeWhile(x => x != string.Empty).ToArray();
 var availableIngredientIds = input.Skip(freshIngredientIds.Length + 1).Select(i => long.Parse(i)).ToArray();
 
-var result = 0;
+var result = 0L;
 
 var freshIngredientRanges = new List<(long, long)>();
 
@@ -18,7 +16,7 @@ foreach (var ingredientIds in freshIngredientIds)
 
 freshIngredientRanges = freshIngredientRanges.OrderBy(r => r.Item1).ToList();
 
-foreach (var ingredientId in availableIngredientIds)
+/*foreach (var ingredientId in availableIngredientIds)
 {
     foreach (var ingredientRange in freshIngredientRanges)
     {
@@ -35,6 +33,35 @@ foreach (var ingredientId in availableIngredientIds)
             break;
         }
     }
+}*/
+
+var inRange = freshIngredientRanges.ToList();
+
+for (var i = 1; i < inRange.Count; i++)
+{
+    for (var j = 0; j < i; j++)
+    {
+        // Check if can extend the end.
+        if ((inRange[i].Item1 >= inRange[j].Item1) && (inRange[i].Item1 <= inRange[j].Item2))
+        {
+            // Start is in the previous range, so can extend the end of it to the current value.
+            if (inRange[i].Item2 > inRange[j].Item2)
+            {
+                Console.WriteLine($"Range {inRange[i].Item1}-{inRange[i].Item2} extends {inRange[j].Item1}-{inRange[j].Item2}");
+
+                var newRange = (inRange[j].Item1, inRange[i].Item2);
+                inRange.RemoveAt(j);
+                inRange.Insert(j, newRange);
+            }
+
+            inRange.RemoveAt(i);
+            i--;
+
+            break;
+        }
+    }
 }
+
+result = inRange.Select(r => (r.Item2 - r.Item1) + 1).Sum();
 
 Console.WriteLine(result);
