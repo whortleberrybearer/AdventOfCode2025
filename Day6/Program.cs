0e7be09a5c4c2825c9d76cdcd8cb554e25c53daf
whortleberrybearer <30476438+whortleberrybearer@users.noreply.github.com>
2025-12-06 17:26:14 +0000

    Part 1

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -1,5 +1,41 @@
 ﻿var input = File.ReadAllLines("Input.txt");
 
-var result = 0;
+var result = 0L;
+
+var grid = input.Take(input.Length - 1).Select(x => x.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(i => long.Parse(i)).ToArray()).ToArray();
+
+var operators = input.Last().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+for (var col = 0; col < operators.Length; col++)
+{
+    var numbers = grid.Where(r => col < r.Length).Select(r => r[col]).ToArray();
+
+    if (operators[col] == "+")
+    {
+        var total = 0L;
+
+        foreach (var value in numbers)
+        {
+            total += value;
+        }
+
+        Console.WriteLine($"Column: {col}, Total: {total}");
+
+        result += total;
+    }
+    else if (operators[col] == "*")
+    {
+        var total = numbers.First();
+
+        foreach (var value in numbers.Skip(1))
+        {
+            total *= value;
+        }
+
+        Console.WriteLine($"Column: {col}, Total: {total}");
+
+        result += total;
+    }
+}
 
 Console.WriteLine(result);
