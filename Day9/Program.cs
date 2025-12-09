89825e3ad49804fb0cd4d7778cf58a21bf5b2cee
whortleberrybearer <30476438+whortleberrybearer@users.noreply.github.com>
2025-12-09 08:57:19 +0000

    Grid rendered

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -2,26 +2,79 @@
 
 var result = 0L;
 
-var coordinates = input.Select(i => i.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(i => long.Parse(i)).ToArray()).ToArray();
-var areas = new List<(string, double)>();
+var coordinates = input.Select(i => i.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(i => int.Parse(i)).ToArray()).ToArray();
+//var areas = new List<(string, double)>();
 
-for (var i = 0; i < coordinates.Length; i++)
+//for (var i = 0; i < coordinates.Length; i++)
+//{
+//    for (var j = 0; j < coordinates.Length; j++)
+//    {
+//        if (i == j)
+//        {
+//            continue;
+//        }
+
+//        var area = (Math.Abs(coordinates[j][0] - coordinates[i][0]) + 1) * (Math.Abs(coordinates[j][1] - coordinates[i][1]) + 1);
+
+//        areas.Add(($"{string.Join(',', coordinates[i])}-{string.Join(',', coordinates[j])}", area));
+//    }
+//}
+
+//areas = areas.OrderByDescending(d => d.Item2).ToList();
+
+//result = (long)areas.First().Item2;
+
+var maxX = coordinates.Max(x => x[0]) + 3;
+var maxY = coordinates.Max(x => x[1]) + 3;
+
+var grid = new char[maxY][];
+
+for (var i = 0; i < maxY; i++)
 {
-    for (var j = 0; j < coordinates.Length; j++)
+    grid[i] = new char[maxX];
+
+    for (var j = 0; j < maxX; j++)
     {
-        if (i == j)
-        {
-            continue;
-        }
-
-        var area = (Math.Abs(coordinates[j][0] - coordinates[i][0]) + 1) * (Math.Abs(coordinates[j][1] - coordinates[i][1]) + 1);
-
-        areas.Add(($"{string.Join(',', coordinates[i])}-{string.Join(',', coordinates[j])}", area));
+        grid[i][j] = '.';
     }
 }
 
-areas = areas.OrderByDescending(d => d.Item2).ToList();
+for (var i = 0; i < coordinates.Length; i++)
+{
+    grid[coordinates[i][1]][coordinates[i][0]] = '#';
+}
 
-result = (long)areas.First().Item2;
+for (var i = 0; i < coordinates.Length; i++)
+{
+    var current = coordinates[i];
+    var next = (i < (coordinates.Length - 1)) ? coordinates[i + 1] : coordinates[0];
+
+    var startX = Math.Min(current[0], next[0]);
+    var endX = Math.Max(current[0], next[0]);
+    var startY = Math.Min(current[1], next[1]);
+    var endY = Math.Max(current[1], next[1]);
+
+    for (var j = startX + 1; j < endX; j++)
+    {
+        grid[startY][j] = 'X';
+    }
+
+    for (var j = startY + 1; j < endY; j++)
+    {
+        grid[j][startX] = 'X';
+    }
+}
+
+for (var i = 0; i < grid.Length; i++)
+{
+    for (var j = 0; j < grid[i].Length; j++)
+    {
+        Console.Write(grid[i][j]);
+    }
+
+    Console.WriteLine();
+}
+
+Console.WriteLine();
 
 Console.WriteLine(result);
