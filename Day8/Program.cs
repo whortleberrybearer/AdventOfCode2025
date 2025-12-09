d8b99731ab86d749c90bfac360eae8672e636097
whortleberrybearer <30476438+whortleberrybearer@users.noreply.github.com>
2025-12-09 00:06:38 +0000

    PArt 2

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -8,12 +8,9 @@ var result = 0L;
 
 var coordinates = input.Select(i => i.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(i => long.Parse(i)).ToArray()).ToArray();
 var distances = new List<(string, double)>();
-var distancesDictionary = new Dictionary<string, (string, double)[]>();
 
 for (var i = 0; i < coordinates.Length; i++)
 {
-    var linkLengths = new List<(string, double)>();
-
     for (var j = 0; j < coordinates.Length; j++)
     {
         if (i == j)
@@ -23,27 +20,15 @@ for (var i = 0; i < coordinates.Length; i++)
 
         var distance = Math.Sqrt(coordinates[i].Zip(coordinates[j], (a, b) => (a - b) * (a - b)).Sum());
 
-        //if (distance < shortest.Item2)
-        //{
-        //    shortest = ($"{string.Join(',', coordinates[i])}-{string.Join(',', coordinates[j])}", distance);
-        //}
-
         distances.Add(($"{string.Join(',', coordinates[i])}-{string.Join(',', coordinates[j])}", distance));
-        linkLengths.Add((string.Join(',', coordinates[j]), distance));
     }
-
-    distancesDictionary.Add(string.Join(',', coordinates[i]), linkLengths.OrderBy(l => l.Item2).ToArray());
 }
 
 distances = distances.OrderBy(d => d.Item2).ToList();
 
 var circuits = input.Select(i => new List<string>() { i }).ToList();
-var connections = 0;
-
-for (var i = 0; i < 2000; i += 2)
+for (var i = 0; i < distances.Count; i += 2)
 {
-    Console.WriteLine($"Connections: {connections}");
-
     var junctionBoxes = distances[i].Item1.Split('-');
 
     var circuit1 = circuits.FirstOrDefault(c => c.Contains(junctionBoxes[0]));
@@ -59,25 +44,16 @@ for (var i = 0; i < 2000; i += 2)
         circuit1.AddRange(circuit2);
         circuits.Remove(circuit2);
 
-        //connections++;
-
-        //if (connections == 999)
-        //{
-        //    break;
-        //}
+        if (circuits.Count == 1)
+        {
+            result = long.Parse(junctionBoxes[0].Split(',')[0]) * long.Parse(junctionBoxes[1].Split(',')[0]);
+            break;
+        }
     }
-
-    //foreach (var additionalDelete in distancesDictionary[junctionBoxes[1]].ToArray())
-    //{
-    //    distances.Remove(($"{}-{}", additionalDelete);
-    //}
 }
 
-var top3 = circuits.OrderByDescending(c => c.Count).Take(3).ToArray();
+//var top3 = circuits.OrderByDescending(c => c.Count).Take(3).ToArray();
 
-result = top3[0].Count * top3[1].Count * top3[2].Count;
+//result = top3[0].Count * top3[1].Count * top3[2].Count;
 
 Console.WriteLine(result);
-
-// 2145 = too low
-// 2688 = too low
