8bdfe7cd31cc196493ec6115a022beb383d272b0
whortleberrybearer <30476438+whortleberrybearer@users.noreply.github.com>
2025-12-10 08:02:44 +0000

    Input read

---
+++ Day10/Program.cs
@@ -0,0 +1,20 @@
+﻿var input = File.ReadAllLines("Input.txt");
+
+var result = 0L;
+
+var machines = new List<(char[] Lights, int[][] Buttons, int[] Joltage)>();
+
+foreach (var line in input)
+{
+    var split = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+    var lights = split[0].Trim('[', ']').ToCharArray();
+    var buttons = split[1..^1].Select(s => s.Trim('(', ')').Split(',').Select(c => int.Parse(c)).ToArray()).ToArray();
+    var joltage = split[^1].Trim('{', '}').Split(',').Select(c => int.Parse(c)).ToArray();
+    
+    machines.Add((lights, buttons, joltage));
+}
+
+
+
+Console.WriteLine(result);
