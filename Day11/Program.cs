605434614d92f1eb5737de1058a21e0fee691168
whortleberrybearer <30476438+whortleberrybearer@users.noreply.github.com>
2025-12-11 12:57:38 +0000

    Parsed input

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -1,5 +1,32 @@
 ﻿var input = File.ReadAllLines("Input.txt");
 
-var result = 0L;s
+var result = 0L;
+
+var devices = new Dictionary<string, Device>();
+
+foreach (var line in input)
+{
+    var split = line.Split(':');
+
+    devices.Add(split[0], new Device() { Name = split[0] });
+}
+
+devices.Add("out", new Device() { Name = "out" });
+
+foreach (var line in input)
+{
+    var split = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+    foreach (var device in split.Skip(1))
+    {
+        devices[split[0].TrimEnd(':')].Outputs.Add(devices[device]);
+    }    
+}
 
 Console.WriteLine(result);
+
+class Device
+{
+    public string Name { get; set; }
+    public List<Device> Outputs = new List<Device>();
+}
