ceefd7a5add401c919f0b5710498cb0ed9c80fa6
whortleberrybearer <30476438+whortleberrybearer@users.noreply.github.com>
2025-12-12 07:03:07 +0000

    Day 11 (part 1 only) (#11)

---
+++ Day11/Program.cs
@@ -0,0 +1,58 @@
+﻿var input = File.ReadAllLines("Input.txt");
+
+var result = 0L;
+
+var devices = new Dictionary<string, Device>();
+
+foreach (var line in input)
+{
+    var split = line.Split(':');
+
+    devices.Add(split[0], new Device() { Name = split[0] });
+}
+
+devices.Add("out", new Device() { Name = "out" });
+
+foreach (var line in input)
+{
+    var split = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+    foreach (var device in split.Skip(1))
+    {
+        devices[split[0].TrimEnd(':')].Outputs.Add(devices[device]);
+    }    
+}
+
+result = devices["you"].FollowOutput(new List<Device>());
+
+Console.WriteLine(result);
+
+class Device
+{
+    public string Name { get; set; }
+    public List<Device> Outputs = new List<Device>();
+
+    internal long FollowOutput(List<Device> previousDevices)
+    {
+        if (previousDevices.Contains(this))
+        {
+            return 0;
+        }
+
+        if (Outputs.Any(o => o.Name == "out"))
+        {
+            Console.WriteLine($"{string.Join("->", previousDevices.Select(d => d.Name))}->{Name}->out");
+
+            return 1;
+        }
+
+        previousDevices = new List<Device>(previousDevices) { this };
+
+        return Outputs.Sum(o => o.FollowOutput(previousDevices));
+    }
+}
+
+class Expansion
+{
+    public Device Device { get; set; }
+}
